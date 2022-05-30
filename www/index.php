46b8942755e8b4ec810df3e810949b04bf4c822b
argjuan85 <argjuan85@gmail.com>
2022-05-30 16:27:41 -0300

    changed header

--- www/index.php
+++ www/index.php
@@ -16,7 +16,7 @@
                 $result = mysqli_query($conn, $query);
 
                 echo '<table class="table table-striped">';
-                echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+                echo '<thead><tr><th></th><th>id</th><th>nombre</th></tr></thead>';
                 while($value = $result->fetch_array(MYSQLI_ASSOC)){
                     echo '<tr>';
                     echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
